f625545217225a9083a3ced5d70085eea637350d
Yothri <admin@yothri.com>
2018-01-17 21:42:39 +0100

    Added Network Project + Added class WPDListener. Not functional yet.

---
+++ Flarine.Network/Web/WPDListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Flarine.Network.Web
+{
+    public abstract class WPDListener : IDisposable
+    {
+        public WPDListener()
+        {
+
+        }
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+
+            }
+        }
+
+    }
+}
