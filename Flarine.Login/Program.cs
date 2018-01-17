2743230e989e974b934d6d9b2e1f058781436eda
Johannes Jacobs <johannesmjacobs@gmail.com>
2018-01-17 13:47:53 +0100

    Removed testing

--- Flarine.Login/Program.cs
+++ Flarine.Login/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using Flarine.Core.Context;
+﻿using Flarine.Core.Context;
 using Flarine.Database;
 
 namespace Flarine.Login
@@ -12,13 +10,7 @@ namespace Flarine.Login
             using (var loginCtx = ContextBase.GetInstance<LoginContext>())
             using (var dbCtx = DatabaseService.GetContext())
             {
-                for (;;)
-                {
-                    foreach (var acc in dbCtx.Accounts.ToList())
-                        Console.WriteLine(acc.Id);
 
-                    Console.ReadLine();
-                }
             }
         }
     }
