3f795e001ddb2b56c5995fb2002cfb0cc8114b76
Yothri <admin@yothri.com>
2018-01-19 11:40:12 +0100

    Changed some ports.

--- Flarine.Login/Config/Model/LoginConfig.cs
+++ Flarine.Login/Config/Model/LoginConfig.cs
@@ -6,7 +6,7 @@ namespace Flarine.Login.Config.Model
     internal sealed class LoginConfig : ContextConfiguration
     {
         public string DatabaseEngine { get; set; } = "SQLite";
-        public string WPDListenPrefix { get; set; } = "http://*:8080/auth/";
+        public string WPDListenPrefix { get; set; } = "http://*:8010/auth/";
         public string ISCServerHost { get; set; } = "192.168.0.122";
         public int ISCServerPort { get; set; } = 8011;
         public Language DefaultLanguageId { get; set; } = Language.English;
