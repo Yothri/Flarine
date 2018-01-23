c3538750a17e039ad1377c44c1c880e51abecc3b
Yothri <admin@yothri.com>
2018-01-23 09:31:29 +0100

    Added SystemSettings to LoginConfiguration.

--- Flarine.Login/Config/Model/LoginConfig.cs
+++ Flarine.Login/Config/Model/LoginConfig.cs
@@ -1,4 +1,5 @@
-﻿using Flarine.Core.Config;
+﻿using System.Collections.Generic;
+using Flarine.Core.Config;
 using Flarine.Core.Context.Model;
 using Flarine.Login.Context.Model;
 
@@ -11,6 +12,13 @@ namespace Flarine.Login.Config.Model
         public string ISCServerHost { get; set; } = "192.168.0.122";
         public int ISCServerPort { get; set; } = 8011;
         public Language DefaultLanguageId { get; set; } = Language.English;
+        public List<SystemSetting> SystemSettings { get; set; } = new List<SystemSetting>
+        {
+            new SystemSetting { Name = "clientTextVersion", Value = "2018011501" },
+            new SystemSetting { Name = "clientVersion", Value = "1.0.12" },
+            new SystemSetting { Name = "isMaintenance", Value = "N" },
+            new SystemSetting { Name = "assetBundleUrl", Value = "http://akamai.mobblo.com/EN/Live17/" }
+        };
         public GameServerGroup[] GameServerGroups { get; set; } = new GameServerGroup[] { new GameServerGroup() { GroupId = 1, Name = "Default", RecommendGameServerId = 1 } };
     }
 }
