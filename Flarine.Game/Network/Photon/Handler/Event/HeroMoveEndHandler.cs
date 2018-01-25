ac839f973e44f0cc0f43d665f619579d01e44032
Yothri <admin@yothri.com>
2018-01-25 20:03:50 +0100

    HeroMoveStart and MoveEnd. No logic yet.

---
+++ Flarine.Game/Network/Photon/Handler/Event/HeroMoveEndHandler.cs
@@ -0,0 +1,16 @@
+﻿using ClientCommon;
+using ClientCommon.ClientEventBody;
+using Flarine.Core.Network.Photon;
+using Flarine.Game.Network.Photon.Common;
+
+namespace Flarine.Game.Network.Photon.Handler.Event
+{
+    [PhotonEventHandler(ClientEventName.kEvent_HeroMoveEnd, typeof(CEBHeroMoveEndEventBody))]
+    internal sealed class HeroMoveEndHandler<T> : OperationHandler<T> where T : CEBHeroMoveEndEventBody
+    {
+        public override void Handle(PhotonGameConnection connection, T requestBody)
+        {
+
+        }
+    }
+}
---
+++ Flarine.Game/Network/Photon/Handler/Event/HeroMoveStartHandler.cs
@@ -0,0 +1,16 @@
+﻿using ClientCommon;
+using ClientCommon.ClientEventBody;
+using Flarine.Core.Network.Photon;
+using Flarine.Game.Network.Photon.Common;
+
+namespace Flarine.Game.Network.Photon.Handler.Event
+{
+    [PhotonEventHandler(ClientEventName.kEvent_HeroMoveStart, typeof(CEBHeroMoveStartEventBody))]
+    internal sealed class HeroMoveStartHandler<T> : OperationHandler<T> where T : CEBHeroMoveStartEventBody
+    {
+        public override void Handle(PhotonGameConnection connection, T requestBody)
+        {
+            
+        }
+    }
+}
